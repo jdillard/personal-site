e8d17d3362d2044cdbad9082788ee060d7c18181
Jared Dillard <jdillard@netgate.com>
2017-05-03 02:32:04 -0500

    update header on all pages

--- assets/js/index.js
+++ assets/js/index.js
@@ -100,3 +100,11 @@ $('#contact-form').submit(function (event) {
     }
   })
 })
+
+var parallaxImage = document.getElementById('ParallaxImage');
+var windowScrolled;
+
+window.addEventListener('scroll', function windowScroll() {
+  windowScrolled = window.pageYOffset || document.documentElement.scrollTop;
+  parallaxImage.style.transform = 'translate3d(0, ' + windowScrolled / 4 + 'px, 0)';
+});
