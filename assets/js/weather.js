e24b45a49f16364bb21de2119bebb95deeaf7cf4
Jared Dillard <jdillard@netgate.com>
2018-03-14 23:13:55 -0500

    add hourly forecasts to crag conditions

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,10 +1,12 @@
 //import 'babel-polyfill';
 
 const days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+const precipitation = ["rain","snow","thunderstorms"];
 
 const template_crag_boilerplate = require("./templates/crag_boilerplate.hbs");
 const template_weather_observations = require("./templates/weather-observations.hbs");
 const template_weather_forecasts = require("./templates/weather-forecasts.hbs");
+const template_weather_hourly = require("./templates/weather-hourly.hbs");
 
 const crags_json = '[{"number": 1, "name": "The Greenbelt", "station": "KATT", "office": "EWX/153,89", "coordinates": [-97.801,30.244]},' +
   '{"number": 2, "name": "Continental Ranch", "station": "KDRT", "office": "EWX/14,74", "coordinates": [-101.44,29.803]},' +
@@ -13,8 +15,8 @@ const crags_json = '[{"number": 1, "name": "The Greenbelt", "station": "KATT", "
   '{"number": 5, "name": "Enchanted Rock", "station": "KT82", "office": "EWX/114,101", "coordinates": [-98.821,30.503]},' +
   '{"number": 6, "name": "Horseshoe Canyon Ranch", "station": "KHRO", "office": "LZK/44,127", "coordinates": [-93.292,36.012]},' +
   '{"number": 7, "name": "Last Chance Canyon", "station": "KGDP", "office": "MAF/16,149", "coordinates": [-104.754,32.234]},' +
-  '{"number": 8, "name": "McKinney Falls", "station": "KAUS", "office": "EWX/156,86", "coordinates": [-97.722,30.181]},' +
-  '{"number": 9, "name": "Georgetown", "station": "KGTU", "office": "EWX/157,106", "coordinates": [-97.69,30.627]}]';
+  '{"number": 8, "name": "Georgetown", "station": "KGTU", "office": "EWX/157,106", "coordinates": [-97.69,30.627]},' +
+  '{"number": 9, "name": "McKinney Falls", "station": "KAUS", "office": "EWX/156,86", "coordinates": [-97.722,30.181]}]';
 
 const crags = JSON.parse(crags_json);
 
@@ -24,7 +26,7 @@ for (let c in crags) {
 
   axios.get('https://api.weather.gov/stations/' + crags[c].station + '/observations')
     .then(function (response) {
-      populateObservations(crags[c].number, crags[c].name, crags[c].coordinates , response.data.features);
+      populateObservations(crags[c], response.data.features);
     })
     .catch(function (error) {
       console.log(error);
@@ -32,7 +34,7 @@ for (let c in crags) {
 
   axios.get('https://api.weather.gov/gridpoints/' + crags[c].office + '/forecast')
     .then(function (response) {
-      populateForecasts(crags[c].number, crags[c].name, crags[c].coordinates , response.data);
+      populateForecasts(crags[c], response.data);
     })
     .catch(function (error) {
       console.log(error);
@@ -92,11 +94,12 @@ function iconToSVG(time, icon) {
     case "few":
     case "wind_few":
     case "sct":
-    case "bkn":
     case "wind_sct":
       return time + "_cloudy";
+    case "bkn":
     case "wind_bkn":
     case "wind_bkn":
+    case "ovc":
       return time + "_overcast";
     case "rain":
     case "rain_showers":
@@ -112,17 +115,16 @@ function iconToSVG(time, icon) {
   }
 }
 
-function populateForecasts(crag_index, crag_name, crag_location, forecasts_data = []) {
+function populateForecasts(crag, data = []) {
   let forecasts = [];
   let half_day = false;
   let clear_forecast = true;
-  const precipitation = ["rain","snow","thunderstorms"];
 
-  forecasts.name = crag_name;
-  forecasts.crag_index = crag_index;
-  forecasts.updated = moment(forecasts_data.properties.updated).format('MM/DD hh:mm a');
-  forecasts.distance = getDistanceFromLatLonInMi(crag_location[1], crag_location[0], forecasts_data.geometry.geometries[0].coordinates[1], forecasts_data.geometry.geometries[0].coordinates[0]).toFixed(2);
-  forecasts.periods = forecasts_data.properties.periods
+  forecasts.name = crag.name;
+  forecasts.crag_index = crag.number;
+  forecasts.updated = moment(data.properties.updated).format('MM/DD hh:mm a');
+  forecasts.distance = getDistanceFromLatLonInMi(crag.coordinates[1], crag.coordinates[0], data.geometry.geometries[0].coordinates[1], data.geometry.geometries[0].coordinates[0]).toFixed(2);
+  forecasts.periods = data.properties.periods
     .reduce(function(a, b, i) {
       let day_icons = [];
       let night_icons = b.icon.substring(35,b.icon.length-12).split("/");
@@ -154,7 +156,6 @@ function populateForecasts(crag_index, crag_name, crag_location, forecasts_data 
           a.icon_split = (a.icon_left === a.icon_right) ? false : a.icon_split;
           a.night_icon_split = (a.night_icon_left === a.night_icon_right) ? false : a.night_icon_split;
           delete a.number;
-          delete a.startTime;
           delete a.endTime;
           delete a.isDaytime;
           delete a.temperatureUnit;
@@ -168,7 +169,7 @@ function populateForecasts(crag_index, crag_name, crag_location, forecasts_data 
       } else {
         day_icons = a[a.length-1].icon.substring(35,a[a.length-1].icon.length-12).split("/");
         if((!half_day && i % 2 === 0) || (half_day && i % 2 != 0)) {
-          if(half_day && i === forecasts_data.properties.periods.length-1) {
+          if(half_day && i === data.properties.periods.length-1) {
             b.night_temperature = null;
             b.night_shortForecast = null;
             b.icon_split = (day_icons.length-1 > 1) ? true: false;
@@ -187,7 +188,6 @@ function populateForecasts(crag_index, crag_name, crag_location, forecasts_data 
             b.icon_split = (b.icon_left === b.icon_right) ? false : b.icon_split;
             b.night_icon_split = (b.night_icon_left === b.night_icon_right) ? false : b.night_icon_split;
             delete b.number;
-            delete b.startTime;
             delete b.endTime;
             delete b.isDaytime;
             delete b.temperatureUnit;
@@ -219,7 +219,6 @@ function populateForecasts(crag_index, crag_name, crag_location, forecasts_data 
           a[a.length-1].icon_split = (a[a.length-1].icon_left === a[a.length-1].icon_right) ? false : a[a.length-1].icon_split;
           a[a.length-1].night_icon_split = (a[a.length-1].night_icon_left === a[a.length-1].night_icon_right) ? false : a[a.length-1].night_icon_split;
           delete a[a.length-1].number;
-          delete a[a.length-1].startTime;
           delete a[a.length-1].endTime;
           delete a[a.length-1].isDaytime;
           delete a[a.length-1].temperatureUnit;
@@ -233,28 +232,115 @@ function populateForecasts(crag_index, crag_name, crag_location, forecasts_data 
     });
   forecasts.color = (clear_forecast) ? 'green' : 'yellow';
 
-  document.getElementById("last-forecast-"+crag_index).innerHTML = forecasts.updated;
-  document.getElementById("office-"+crag_index).innerHTML = forecasts.distance + " miles";
-  document.getElementById("forecast-"+crag_index).classList.add("b--" + forecasts.color);
-  document.getElementById("forecast-"+crag_index).innerHTML = template_weather_forecasts(forecasts);
+  document.getElementById("last-forecast-"+crag.number).innerHTML = forecasts.updated;
+  document.getElementById("office-"+crag.number).innerHTML = forecasts.distance + " miles";
+  document.getElementById("forecast-"+crag.number).innerHTML = template_weather_forecasts(forecasts);
+
+  axios.get('https://api.weather.gov/gridpoints/' + crag.office + '/forecast/hourly')
+  .then(function (response) {
+    populateHourlyForecasts(crag.number, forecasts.periods[0].startTime, response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 }
 
-function populateObservations(crag_index, crag_name, crag_location, observations_data = []) {
+function populateHourlyForecasts(crag_index, week_start_time, data) {
+  let hourly = [];
+  //TODO make pretty
+  let arr = [];
+      arr[0] = [];
+      arr[1] = [];
+      arr[2] = [];
+      arr[3] = [];
+      arr[4] = [];
+      arr[5] = [];
+      arr[6] = [];
+
+  hourly.days = data.properties.periods
+    .reduce(function(a, b, i) {
+      let left_icons = [];
+      let right_icons = b.icon.substring(35,b.icon.length-11).split("/");
+      if (i === 1) {
+        left_icons = a.icon.substring(35,a.icon.length-11).split("/");
+        delete a.number;
+        delete a.detailedForecast;
+        delete a.name;
+        delete a.endTime;
+        delete a.temperatureTrend;
+        a.hour = moment(a.startTime).format('HH');
+        a.svg = iconToSVG(left_icons[0], left_icons[1].split(",")[0]);
+        a.color = (new RegExp(precipitation.join("|")).test(a.svg)) ? 'yellow' : 'green';
+        if(moment(a.startTime).isSameOrAfter(moment(week_start_time), 'day')) {
+          arr[days.indexOf(moment(a.startTime).format('dddd').toLowerCase())].push(a);
+        }
+        delete b.number;
+        delete b.detailedForecast;
+        delete b.name;
+        delete b.endTime;
+        delete b.temperatureTrend;
+        b.hour = moment(b.startTime).format('HH');
+        b.svg = iconToSVG(right_icons[0], right_icons[1].split(",")[0]);
+        b.color = (new RegExp(precipitation.join("|")).test(b.svg)) ? 'yellow' : 'green';
+        if(moment(b.startTime).isSameOrAfter(moment(week_start_time), 'day')) {
+          arr[days.indexOf(moment(b.startTime).format('dddd').toLowerCase())].push(b);
+        }
+        return arr;
+      } else {
+        delete b.number;
+        delete b.detailedForecast;
+        delete b.name;
+        delete b.endTime;
+        delete b.temperatureTrend;
+        b.hour = moment(b.startTime).format('HH');
+        b.svg = iconToSVG(right_icons[0], right_icons[1].split(",")[0]);
+        b.color = (new RegExp(precipitation.join("|")).test(b.svg)) ? 'yellow' : 'green';
+        if(moment(b.startTime).isSameOrAfter(moment(week_start_time), 'day')) {
+          a[days.indexOf(moment(b.startTime).format('dddd').toLowerCase())].push(b);
+        }
+        return a;
+      }
+    });
+
+  // if less than 24, fill in the blanks with na.svg and delete the one that don't matter
+  for (const [i, value] of hourly.days.entries()) {
+    if(value.length < 24) {
+      value.sort(function(a, b) { return +a.hour - +b.hour; });
+      const last_hour = +value[value.length-1].hour;
+      for (let i=last_hour+1; i < 24; i++) {
+        let isDaytime = (i < 6 || i > 17) ? false : true;
+        let svg = (i < 6 || i > 17) ? 'night_na' : 'day_na';
+        value.push({
+          'hour': i.toString().padStart(2, 0),
+          'svg': svg,
+          'isDaytime': isDaytime,
+          'shortForecast': 'Not Available',
+          'temperature': null,
+          'color': 'light-gray'
+        });
+      }
+    }
+  }
+
+  localStorage.setItem(crag_index, JSON.stringify(hourly.days));
+}
+
+function populateObservations(crag, data = []) {
   let observations = [];
 
-  observations.name = crag_name;
-  observations.crag_index = crag_index;
-  observations.distance = getDistanceFromLatLonInMi(crag_location[1], crag_location[0], observations_data[0].geometry.coordinates[1], observations_data[0].geometry.coordinates[0]).toFixed(2);
-  observations.curr_timestamp = moment(observations_data[0].properties.timestamp).format('MM/DD hh:mm a');
-  observations.curr_temp = (observations_data[0].properties.temperature.value*(9/5)+32).toFixed(0);
-  observations.curr_heat_index = (observations_data[0].properties.heatIndex.value && observations.curr_temp != (observations_data[0].properties.heatIndex.value*(9/5)+32).toFixed(0)) ? (observations_data[0].properties.heatIndex.value*(9/5)+32).toFixed(0) : null;
-  observations.curr_wind = (observations_data[0].properties.windSpeed.value) ? (observations_data[0].properties.windSpeed.value * 2.23694).toFixed(0) + "mph " + degreeToDirection(observations_data[0].properties.windDirection.value) : null;
-  observations.curr_wind_chill = (observations_data[0].properties.windChill.value) ? (observations_data[0].properties.windChill.value*(9/5)+32).toFixed(0) : null;
-  observations.curr_humidity = (observations_data[0].properties.relativeHumidity.value) ? observations_data[0].properties.relativeHumidity.value.toFixed(0) : null;
+  observations.name = crag.name;
+  observations.crag_index = crag.number;
+  observations.distance = getDistanceFromLatLonInMi(crag.coordinates[1], crag.coordinates[0], data[0].geometry.coordinates[1], data[0].geometry.coordinates[0]).toFixed(2);
+  observations.curr_timestamp = moment(data[0].properties.timestamp).format('MM/DD hh:mm a');
+  observations.curr_temp = (data[0].properties.temperature.value*(9/5)+32).toFixed(0);
+  observations.curr_heat_index = (data[0].properties.heatIndex.value && observations.curr_temp != (data[0].properties.heatIndex.value*(9/5)+32).toFixed(0)) ? (data[0].properties.heatIndex.value*(9/5)+32).toFixed(0) : null;
+  observations.curr_wind = (data[0].properties.windSpeed.value) ? (data[0].properties.windSpeed.value * 2.23694).toFixed(0) + "mph " + degreeToDirection(data[0].properties.windDirection.value) : null;
+  observations.curr_wind_chill = (data[0].properties.windChill.value) ? (data[0].properties.windChill.value*(9/5)+32).toFixed(0) : null;
+  observations.curr_humidity = (data[0].properties.relativeHumidity.value) ? data[0].properties.relativeHumidity.value.toFixed(0) : null;
 
   let total_inches = 0;
 
-  const precips = observations_data.slice(0, 48)
+  const precips = data.slice(0, 48)
   .map(i => {
     total_inches += +i.properties.precipitationLastHour.value;
     return {
@@ -263,7 +349,7 @@ function populateObservations(crag_index, crag_name, crag_location, observations
     };
   });
 
-  if(observations_data[0].properties.precipitationLastHour.value > 0) {
+  if(data[0].properties.precipitationLastHour.value > 0) {
     observations.color = 'light-red';
   } else if(total_inches > 0) {
     observations.color = 'yellow';
@@ -271,11 +357,12 @@ function populateObservations(crag_index, crag_name, crag_location, observations
     observations.color = 'green';
   }
 
-  document.getElementById("crag-"+crag_index).classList.add("b--" + observations.color);
-  document.getElementById("observation-"+crag_index).innerHTML += template_weather_observations(observations);
-  graph_precip(crag_index, precips);
+  document.getElementById("crag-"+crag.number).classList.add("b--" + observations.color);
+  document.getElementById("observation-"+crag.number).innerHTML += template_weather_observations(observations);
+  graph_precip(crag.number, precips);
 }
 
+//TODO make responsive
 function graph_precip(crag_index, data) {
   const svg = d3.select("#precip-"+crag_index);
   const margin = {top: 20, right: 15, bottom: 20, left: 15};
@@ -321,3 +408,60 @@ function graph_precip(crag_index, data) {
     .attr("stroke-width", 1.5)
     .attr("d", line);
 }
+
+$(document).on( "click", '.forecast-day', function() {
+  const crag_index = $(this)[0].dataset.crag;
+  const day_index = $(this)[0].dataset.day;
+  const is_active = ($(this)[0].dataset.active == 'true');
+
+  let hourly_forecast = (window.outerWidth > 480) ? document.getElementById('hourly-forecast-'+crag_index) : document.getElementById('hourly-forecast-'+crag_index+'-'+day_index);
+  let daytime = document.getElementById(crag_index+'-daytime-'+day_index);
+  let nighttime = document.getElementById(crag_index+'-nighttime-'+day_index);
+  if(is_active) {
+    hourly_forecast.classList.remove('flex');
+    hourly_forecast.classList.add('dn');
+    $(this).removeClass('bg-light-gray');
+    $(this)[0].dataset.active = false;
+    daytime.classList.add('bg-white');
+    daytime.classList.remove('bg-near-white');
+    nighttime.classList.add('bg-mid-gray');
+    nighttime.classList.remove('bg-gray');
+  } else {
+    $('#forecast-'+crag_index+' .forecast-day').each(function(i, obj) {
+      $(this)[0].dataset.active = false;
+      $(this).removeClass('bg-light-gray');
+    });
+    $(this).addClass('bg-light-gray');
+    $(this)[0].dataset.active = true;
+    hourly_forecast.classList.remove('dn');
+    hourly_forecast.classList.add('flex');
+    $('#forecast-'+crag_index+' .daytime').each(function(i, obj) {
+      $(this).removeClass('bg-near-white');
+      $(this).addClass('bg-white');
+    });
+    daytime.classList.remove('bg-white');
+    daytime.classList.add('bg-near-white');
+    $('#forecast-'+crag_index+' .nighttime').each(function(i, obj) {
+      $(this).removeClass('bg-gray');
+      $(this).addClass('bg-mid-gray');
+    });
+    nighttime.classList.remove('bg-mid-gray');
+    nighttime.classList.add('bg-gray');
+  }
+
+  const hourly = JSON.parse(localStorage.getItem(crag_index));
+
+  hourly_forecast.innerHTML = template_weather_hourly(hourly[days.indexOf($(this).text().toLowerCase())]);
+});
+
+//TODO unset all hourly settings on all crags on window resize
+
+/*
+//TODO remove localStrage on tab closure
+window.onbeforeunload = function() {
+  for (let crag in crags) {
+    localStorage.removeItem(crag.number);
+  }
+  return '';
+};
+*/
