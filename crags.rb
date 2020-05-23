16358d482816510393f536cc3c1c63ed9a05846d
Jared Dillard <jared.dillard@gmail.com>
2020-05-23 12:59:57 -0700

    Clean up ruby warnings

--- crags.rb
+++ crags.rb
@@ -6,7 +6,6 @@ def create_crag_page(crags)
   # determine unique metros
   metros = Set[]
   states = Set[]
-  local_crags = Hash.new
   state_locals = Hash.new
 
   #TODO needs to loop through each metros cell
@@ -63,105 +62,105 @@ def create_crag_page(crags)
 end
 
 def create_crags(crags)
-    # determine unique metros
-    metros = Set[]
-    local_crags = Hash.new
+  # determine unique metros
+  metros = Set[]
+  local_crags = Hash.new
 
-    #TODO needs to loop through each metros cell
-    crags.each do |crag|
-      crag["metros"].split("|").each do |metro|
-        metros.add(metro)
-      end 
+  #TODO needs to loop through each metros cell
+  crags.each do |crag|
+    crag["metros"].split("|").each do |metro|
+      metros.add(metro)
     end
+  end
 
-    # determine the crags near them
-    metros.each do |metro|
-      local_crags[metro] = [];
+  # determine the crags near them
+  metros.each do |metro|
+    local_crags[metro] = [];
+  end
+
+  crags.each do |crag|
+    crag["metros"].split("|").each do |metro|
+      local_crags[metro].push({ name: crag["name"], state: crag["state"], note: crag["note"], url: crag["url"], station: crag["station"], office: crag["office"], lat: crag["lat"], long: crag["long"] })
     end
+  end
 
-    crags.each do |crag|
-      crag["metros"].split("|").each do |metro|
-        local_crags[metro].push({ name: crag["name"], state: crag["state"], note: crag["note"], url: crag["url"], station: crag["station"], office: crag["office"], lat: crag["lat"], long: crag["long"] })
-      end
-    end
-
-    crags.each do |crag|
-      slug = crag["name"].gsub(' ', '-').gsub(/[^\w-]/, '').gsub(/(-){2,}/, '-').downcase + "-" + crag["state"].gsub(' ', '-').gsub(/[^\w-]/, '').gsub(/(-){2,}/, '-').downcase
-      File.open("source/_crags/" + slug + "-weather.md","w") do |f|
-        f << "---\n"
-        f << "### THIS FILE IS AUTO-GENERATED - DO NOT EDIT ###\n"
-        f << "layout: page\n"
-        f << "title: " + crag["name"] + ", " + crag["state"] + " Climbing Weather - Current, Past, and Forecasted Report\n"
-        f << "title_override: " + crag["name"] + "<br /><small>Climbing Weather</small>\n"
-        f << "description: A lightweight weather report for " + crag["name"] + ', ' + crag["state"] + ". Optimized for slow internet connections.\n"
-        f << "js_includes:\n"
-        f << "  - weather.js\n"
-        f << "---\n\n"
-        f << '<section class="measure center lh-copy f5-ns f6 ph2 mv4" style="text-align: justify;">'+"\n"
-        f << '<strong>"Is it dry?"</strong>, an oft-repeated, age-old question. Here are real-time,'+"\n"
-        f << 'precipitation-focused reports of current, past, and forecasted climbing weather for ' + crag["name"] + ', ' + crag["state"] + '.'+"\n"
-        # f << 'If it is too wet here, check for a dry crag near'
-        # local_crags.each do |metro, crags|
-        #   if metro == crag["metros"]
-        #     url = metro.gsub(' ', '-').gsub(/[^\w-]/, '').gsub(/(-){2,}/, '-').downcase
-        #     f << ' <a class="nowrap no-underline fancy-link relative light-red" href="/crags/' + url + '-weather.html">' + metro + '</a>'
-        #   end
-        # end
-        # f << " and keep the stoke high.\n"
-        f << "</section>\n\n"
-        f << '<p id="settings-toggle" class="mw5 b center tc hover-light-red black-70 pointer">Show Instructions</p>'+"\n"
-        f << '<section id="settings" class="overflow-hidden" style="display:none;">'+"\n"
-        f << '    <div class="mv2 ph2 center">'+"\n"
-        f << '        <div class="fn f6 tc pv2">'+"\n"
-        f << '            <p class="measure lh-copy center"><strong>Show/hide hourly forecasts</strong> by clicking the desired day.</p>'+"\n"
-        f << '            <hr class="mw5 p0 mv2 o-60 b0 bt b--light-red light-red bg-light-red">'+"\n"
-        f << '            <p class="measure lh-copy center"><strong>Current and Past conditions</strong> are measured by the nearest weather station. <strong>Forecast conditions</strong> are calculated and polled separately.</p>'+"\n"
-        f << '            <hr class="mw5 p0 mv2 o-60 b0 bt b--light-red light-red bg-light-red">'+"\n"
-        f << '            <p class="measure lh-copy center"><strong>Having issues?</strong> Try <a id="clear-cache" class="no-underline relative fancy-link light-red hover-light-red" href="#">clearing the local cache</a>.</p>'+"\n"
-        f << '            <hr class="mw5 p0 mv2 o-60 b0 bt b--light-red light-red bg-light-red">'+"\n"
-        f << '            <p class="measure lh-copy center">Weather data sourced from <a class="no-underline fancy-link relative light-red" target="_blank" href="https://www.weather.gov/documentation/services-web-api">weather.gov</a>.</p>'+"\n"
-        f << "        </div>\n"
-        f << "    </div>\n"
-        f << "</section>\n"
-        f << '<section id="weather" data-crag="' + slug + '" class="mv4-ns mv3 ph2 center"></section>'+"\n"
-        f << '<section id="nearby" class="tc lh-copy">'+"\n"
-        f << '  <h3>Nearby Crags</h3>'+"\n"
-        local_crags.each do |metro, crags|
-          crag["metros"].split("|").each do |metro2|
-            if metro == metro2
-              crags.each do |crag2|
-                if crag2[:name] != crag["name"]
-                  url = crag2[:name].gsub(' ', '-').gsub(/[^\w-]/, '').gsub(/(-){2,}/, '-').downcase + "-" + crag2[:state].gsub(' ', '-').gsub(/[^\w-]/, '').gsub(/(-){2,}/, '-').downcase
-                  f << '<a class="nowrap no-underline fancy-link relative light-red mh3" href="/crags/' + url + '-weather.html">' + crag2[:name] + '</a>'+"\n"
-                end
+  crags.each do |crag|
+    slug = crag["name"].gsub(' ', '-').gsub(/[^\w-]/, '').gsub(/(-){2,}/, '-').downcase + "-" + crag["state"].gsub(' ', '-').gsub(/[^\w-]/, '').gsub(/(-){2,}/, '-').downcase
+    File.open("source/_crags/" + slug + "-weather.md","w") do |f|
+      f << "---\n"
+      f << "### THIS FILE IS AUTO-GENERATED - DO NOT EDIT ###\n"
+      f << "layout: page\n"
+      f << "title: " + crag["name"] + ", " + crag["state"] + " Climbing Weather - Current, Past, and Forecasted Report\n"
+      f << "title_override: " + crag["name"] + "<br /><small>Climbing Weather</small>\n"
+      f << "description: A lightweight weather report for " + crag["name"] + ', ' + crag["state"] + ". Optimized for slow internet connections.\n"
+      f << "js_includes:\n"
+      f << "  - weather.js\n"
+      f << "---\n\n"
+      f << '<section class="measure center lh-copy f5-ns f6 ph2 mv4" style="text-align: justify;">'+"\n"
+      f << '<strong>"Is it dry?"</strong>, an oft-repeated, age-old question. Here are real-time,'+"\n"
+      f << 'precipitation-focused reports of current, past, and forecasted climbing weather for ' + crag["name"] + ', ' + crag["state"] + '.'+"\n"
+      # f << 'If it is too wet here, check for a dry crag near'
+      # local_crags.each do |metro, crags|
+      #   if metro == crag["metros"]
+      #     url = metro.gsub(' ', '-').gsub(/[^\w-]/, '').gsub(/(-){2,}/, '-').downcase
+      #     f << ' <a class="nowrap no-underline fancy-link relative light-red" href="/crags/' + url + '-weather.html">' + metro + '</a>'
+      #   end
+      # end
+      # f << " and keep the stoke high.\n"
+      f << "</section>\n\n"
+      f << '<p id="settings-toggle" class="mw5 b center tc hover-light-red black-70 pointer">Show Instructions</p>'+"\n"
+      f << '<section id="settings" class="overflow-hidden" style="display:none;">'+"\n"
+      f << '    <div class="mv2 ph2 center">'+"\n"
+      f << '        <div class="fn f6 tc pv2">'+"\n"
+      f << '            <p class="measure lh-copy center"><strong>Show/hide hourly forecasts</strong> by clicking the desired day.</p>'+"\n"
+      f << '            <hr class="mw5 p0 mv2 o-60 b0 bt b--light-red light-red bg-light-red">'+"\n"
+      f << '            <p class="measure lh-copy center"><strong>Current and Past conditions</strong> are measured by the nearest weather station. <strong>Forecast conditions</strong> are calculated and polled separately.</p>'+"\n"
+      f << '            <hr class="mw5 p0 mv2 o-60 b0 bt b--light-red light-red bg-light-red">'+"\n"
+      f << '            <p class="measure lh-copy center"><strong>Having issues?</strong> Try <a id="clear-cache" class="no-underline relative fancy-link light-red hover-light-red" href="#">clearing the local cache</a>.</p>'+"\n"
+      f << '            <hr class="mw5 p0 mv2 o-60 b0 bt b--light-red light-red bg-light-red">'+"\n"
+      f << '            <p class="measure lh-copy center">Weather data sourced from <a class="no-underline fancy-link relative light-red" target="_blank" href="https://www.weather.gov/documentation/services-web-api">weather.gov</a>.</p>'+"\n"
+      f << "        </div>\n"
+      f << "    </div>\n"
+      f << "</section>\n"
+      f << '<section id="weather" data-crag="' + slug + '" class="mv4-ns mv3 ph2 center"></section>'+"\n"
+      f << '<section id="nearby" class="tc lh-copy">'+"\n"
+      f << '  <h3>Nearby Crags</h3>'+"\n"
+      local_crags.each do |metro, crags|
+        crag["metros"].split("|").each do |metro2|
+          if metro == metro2
+            crags.each do |crag2|
+              if crag2[:name] != crag["name"]
+                url = crag2[:name].gsub(' ', '-').gsub(/[^\w-]/, '').gsub(/(-){2,}/, '-').downcase + "-" + crag2[:state].gsub(' ', '-').gsub(/[^\w-]/, '').gsub(/(-){2,}/, '-').downcase
+                f << '<a class="nowrap no-underline fancy-link relative light-red mh3" href="/crags/' + url + '-weather.html">' + crag2[:name] + '</a>'+"\n"
               end
             end
           end
         end
-        f << '</section>'+"\n"
-        f << "{% include feedback.html %}\n"
-        f << '<p id="issues-toggle" class="mw5 b center tc hover-light-red black-70 pointer">Show Known Issues</p>'+"\n"
-        f << '<section id="issues" class="overflow-hidden tc f6">'+"\n"
-        f << "</section>\n\n"
       end
+      f << '</section>'+"\n"
+      f << "{% include feedback.html %}\n"
+      f << '<p id="issues-toggle" class="mw5 b center tc hover-light-red black-70 pointer">Show Known Issues</p>'+"\n"
+      f << '<section id="issues" class="overflow-hidden tc f6">'+"\n"
+      f << "</section>\n\n"
+    end
 
-      File.open("source/assets/json/crags/" + slug + ".json","w") do |f|
-        f << "[\n"
-        f << "  {\n"
-        f << '    "name": "' + crag["name"] + '",'+"\n"
-        f << '    "note": "' + crag["note"] + '",'+"\n"
-        f << '    "mountainProject": "' + crag["url"] + '",'+"\n"
-        f << '    "station": "' + crag["station"] + '",'+"\n"
-        f << '    "office": "' + crag["office"] + '",'+"\n"
-        f << '    "coordinates": ['+"\n"
-        f << "      " + crag["lat"] + ",\n"
-        f << "      " + crag["long"] + "\n"
-        f << "    ]\n"
-        f << "  }\n"
-        f << "]"
-      end
+    File.open("source/assets/json/crags/" + slug + ".json","w") do |f|
+      f << "[\n"
+      f << "  {\n"
+      f << '    "name": "' + crag["name"] + '",'+"\n"
+      f << '    "note": "' + crag["note"] + '",'+"\n"
+      f << '    "mountainProject": "' + crag["url"] + '",'+"\n"
+      f << '    "station": "' + crag["station"] + '",'+"\n"
+      f << '    "office": "' + crag["office"] + '",'+"\n"
+      f << '    "coordinates": ['+"\n"
+      f << "      " + crag["lat"] + ",\n"
+      f << "      " + crag["long"] + "\n"
+      f << "    ]\n"
+      f << "  }\n"
+      f << "]"
     end
   end
+end
 
 def create_metros(crags)
   # determine unique metros
@@ -216,52 +215,52 @@ def create_metros(crags)
     slug = metro.gsub(' ', '-').gsub(/[^\w-]/, '').gsub(/(-){2,}/, '-').downcase
     File.open("source/_crags/" + slug + "-weather.md","w") do |f|
       f << "---\n"
-        f << "### THIS FILE IS AUTO-GENERATED - DO NOT EDIT ###\n"
-        f << "layout: page\n"
-        f << "title: " + metro + " Climbing Weather - Current, Past, and Forecasted Report\n"
-        f << "title_override: " + metro + "<br /><small>Climbing Weather</small>\n"
-        f << "description: A lightweight climbing weather report for crags near " + metro + ". Optimized for slow internet connections.\n"
-        f << "js_includes:\n"
-        f << "  - weather.js\n"
-        f << "---\n\n"
-        f << '<section class="measure center lh-copy f5-ns f6 ph2 mv4" style="text-align: justify;">'+"\n"
-        f << '<strong>"Is it dry?"</strong>, an oft-repeated, age-old question. Here are real-time,'+"\n"
-        f << 'precipitation-focused reports of current, past, and forecasted climbing weather for crags near ' + metro + ', sourced'+"\n"
-        f << 'from <a class="no-underline fancy-link relative light-red" target="_blank" href="https://www.weather.gov/documentation/services-web-api">weather.gov</a>.'+"\n"
-        f << "</section>\n\n"
-        f << '<p id="settings-toggle" class="mw5 b center tc hover-light-red black-70 pointer">Show Settings</p>'+"\n"
-        f << '<section id="settings" class="overflow-hidden" style="display:none;">'+"\n"
-        f << '    <div class="mv2 ph2 center">'+"\n"
-        f << '        <div id="menu" class="fn fl-ns w-50-l w-100 pv2 pr4-l">'+"\n"
-        f << '            <div class="f7 tc b">Select Defaults:</div>'+"\n"
-        f << "        </div>\n"
-        f << '        <div class="fn f6 tc fl-ns w-50-l w-100 pv2">'+"\n"
-        f << '            <span class="f7 b">Instructions:</span>'+"\n"
-        f << '            <p class="measure lh-copy center"><strong>Show/hide crags</strong> by clicking on their name to the left; green mean shown and gray means hidden.</p>'+"\n"
-        f << '            <hr class="mw5 p0 mv2 o-60 b0 bt b--light-red light-red bg-light-red">'+"\n"
-        f << '            <p class="measure lh-copy center"><strong>Show/hide hourly forecasts</strong> by clicking the desired day.</p>'+"\n"
-        f << '            <hr class="mw5 p0 mv2 o-60 b0 bt b--light-red light-red bg-light-red">'+"\n"
-        f << '            <p class="measure lh-copy center"><strong>Current and Past conditions</strong> are measured by the nearest weather station. <strong>Forecast conditions</strong> are calculated and polled separately.</p>'+"\n"
-        f << '            <hr class="mw5 p0 mv2 o-60 b0 bt b--light-red light-red bg-light-red">'+"\n"
-        f << '            <p class="measure lh-copy center"><strong>Having issues?</strong> Try <a id="clear-cache" class="no-underline relative fancy-link light-red hover-light-red" href="#">clearing the local cache</a>.</p>'+"\n"
-        f << "        </div>\n"
-        f << "    </div>\n"
-        f << "</section>\n"
-        f << '<section id="weather" data-crag="' + slug + '" class="mv4-ns mv3 ph2 center"></section>'+"\n"
-        f << '<section id="nearby" class="tc lh-copy">'+"\n"
-        f << '  <h3>Other Metros</h3>'+"\n"
-        metros.each do |othermetro|
-              url = othermetro.gsub(' ', '-').gsub(/[^\w-]/, '').gsub(/(-){2,}/, '-').downcase
-              f << '<a class="nowrap no-underline fancy-link relative light-red mh3" href="/crags/' + url + '-weather.html">' + othermetro + '</a>'+"\n"
-        end
-        f << '</section>'+"\n"
-        f << "{% include feedback.html %}\n"
-        f << '<p id="issues-toggle" class="mw5 b center tc hover-light-red black-70 pointer">Show Known Issues</p>'+"\n"
-        f << '<section id="issues" class="overflow-hidden tc f6">'+"\n"
-        f << "</section>\n\n"
+      f << "### THIS FILE IS AUTO-GENERATED - DO NOT EDIT ###\n"
+      f << "layout: page\n"
+      f << "title: " + metro + " Climbing Weather - Current, Past, and Forecasted Report\n"
+      f << "title_override: " + metro + "<br /><small>Climbing Weather</small>\n"
+      f << "description: A lightweight climbing weather report for crags near " + metro + ". Optimized for slow internet connections.\n"
+      f << "js_includes:\n"
+      f << "  - weather.js\n"
+      f << "---\n\n"
+      f << '<section class="measure center lh-copy f5-ns f6 ph2 mv4" style="text-align: justify;">'+"\n"
+      f << '<strong>"Is it dry?"</strong>, an oft-repeated, age-old question. Here are real-time,'+"\n"
+      f << 'precipitation-focused reports of current, past, and forecasted climbing weather for crags near ' + metro + ', sourced'+"\n"
+      f << 'from <a class="no-underline fancy-link relative light-red" target="_blank" href="https://www.weather.gov/documentation/services-web-api">weather.gov</a>.'+"\n"
+      f << "</section>\n\n"
+      f << '<p id="settings-toggle" class="mw5 b center tc hover-light-red black-70 pointer">Show Settings</p>'+"\n"
+      f << '<section id="settings" class="overflow-hidden" style="display:none;">'+"\n"
+      f << '    <div class="mv2 ph2 center">'+"\n"
+      f << '        <div id="menu" class="fn fl-ns w-50-l w-100 pv2 pr4-l">'+"\n"
+      f << '            <div class="f7 tc b">Select Defaults:</div>'+"\n"
+      f << "        </div>\n"
+      f << '        <div class="fn f6 tc fl-ns w-50-l w-100 pv2">'+"\n"
+      f << '            <span class="f7 b">Instructions:</span>'+"\n"
+      f << '            <p class="measure lh-copy center"><strong>Show/hide crags</strong> by clicking on their name to the left; green mean shown and gray means hidden.</p>'+"\n"
+      f << '            <hr class="mw5 p0 mv2 o-60 b0 bt b--light-red light-red bg-light-red">'+"\n"
+      f << '            <p class="measure lh-copy center"><strong>Show/hide hourly forecasts</strong> by clicking the desired day.</p>'+"\n"
+      f << '            <hr class="mw5 p0 mv2 o-60 b0 bt b--light-red light-red bg-light-red">'+"\n"
+      f << '            <p class="measure lh-copy center"><strong>Current and Past conditions</strong> are measured by the nearest weather station. <strong>Forecast conditions</strong> are calculated and polled separately.</p>'+"\n"
+      f << '            <hr class="mw5 p0 mv2 o-60 b0 bt b--light-red light-red bg-light-red">'+"\n"
+      f << '            <p class="measure lh-copy center"><strong>Having issues?</strong> Try <a id="clear-cache" class="no-underline relative fancy-link light-red hover-light-red" href="#">clearing the local cache</a>.</p>'+"\n"
+      f << "        </div>\n"
+      f << "    </div>\n"
+      f << "</section>\n"
+      f << '<section id="weather" data-crag="' + slug + '" class="mv4-ns mv3 ph2 center"></section>'+"\n"
+      f << '<section id="nearby" class="tc lh-copy">'+"\n"
+      f << '  <h3>Other Metros</h3>'+"\n"
+      metros.each do |othermetro|
+            url = othermetro.gsub(' ', '-').gsub(/[^\w-]/, '').gsub(/(-){2,}/, '-').downcase
+            f << '<a class="nowrap no-underline fancy-link relative light-red mh3" href="/crags/' + url + '-weather.html">' + othermetro + '</a>'+"\n"
       end
+      f << '</section>'+"\n"
+      f << "{% include feedback.html %}\n"
+      f << '<p id="issues-toggle" class="mw5 b center tc hover-light-red black-70 pointer">Show Known Issues</p>'+"\n"
+      f << '<section id="issues" class="overflow-hidden tc f6">'+"\n"
+      f << "</section>\n\n"
     end
   end
+end
 
 crags = CSV.read("crags.csv", headers: true)
 
