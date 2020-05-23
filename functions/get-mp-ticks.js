5a36bd8fb6d329275e4f3f6d90c9aa2eb07bb638
Jared Dillard <jared.dillard@gmail.com>
2020-05-23 16:12:18 -0700

    Make get-mp-ticks functional

--- functions/get-mp-ticks.js
+++ functions/get-mp-ticks.js
@@ -1,9 +1,15 @@
+const axios = require("axios");
+
 exports.handler = async (event, context, callback) => {
   const MP_KEY = process.env
-  try {
-    const email = event.queryStringParameters.email || "World"
-    return { statusCode: 200, body: JSON.stringify({ msg: `Hello ${email}` }) }
-  } catch (err) {
-    return { statusCode: 500, body: err.toString() }
-  }
+  const email = event.queryStringParameters.email
+
+  Promise.all([
+    axios.get(`https://www.mountainproject.com/data/get-ticks?email=${email}&key=${MP_KEY}`),
+    axios.get(`https://www.mountainproject.com/data/get-user?email=${email}&key=${MP_KEY}`)
+  ]).then(([ticksResponse, userResponse]) => {
+    return { statusCode: 200, body: JSON.stringify({ name: userResponse.data.name, ticks:  ticksResponse.data.ticks }) }
+  }, (error) => {
+    return { statusCode: 500, body: error }
+  });
 }
