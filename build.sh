13aa118df2a0df6a60b03f810ad3447c9d5815fd
Jared Dillard <jared.dillard@gmail.com>
2020-05-23 14:58:57 -0700

    Add script handling to build script

--- build.sh
+++ build.sh
@@ -2,13 +2,24 @@
 
 env
 
+function urldecode() { : "${*//+/ }"; echo -e "${_//%/\\x}"; }
+
 if [ "${INCOMING_HOOK_BODY}" == "" ] ; then
   bundle exec jekyll build --source source
   exit 0
 else
-  SCRIPTS=$(echo "${INCOMING_HOOK_BODY}" | jq -r .scripts)
-  echo "${SCRIPTS}"
-  ruby ./crags.rb
+  PAYLOAD=$(urldecode "${INCOMING_HOOK_BODY}")
+  SCRIPTS_STRING=$(echo "${PAYLOAD}" | jq -r .scripts)
+  IFS='|' read -r -a SCRIPTS <<< "$SCRIPTS_STRING"
+
+  for i in "${SCRIPTS[@]}" ; do
+    case "$i" in
+      crags)
+        echo "ruby crags.rb"
+        ;;
+    esac
+  done
+
   bundle exec jekyll build --source source
 fi
 
