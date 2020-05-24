348b749196199329949bf59a7aeaa2f24b8dd144
Jared Dillard <jared.dillard@gmail.com>
2020-05-23 19:26:18 -0700

    Fix env varariable?

--- functions/get-mp-ticks.js
+++ functions/get-mp-ticks.js
@@ -2,13 +2,12 @@ const axios = require("axios");
 
 exports.handler = async (event, context) => {
   try {
-    const MP_KEY = process.env
     const email = event.queryStringParameters.email || ""
 
     if(email) {
       const [ticksResponse, userResponse] = await Promise.all([
-        axios.get(`https://www.mountainproject.com/data/get-ticks?email=${email}&key=${MP_KEY}`),
-        axios.get(`https://www.mountainproject.com/data/get-user?email=${email}&key=${MP_KEY}`)
+        axios.get(`https://www.mountainproject.com/data/get-ticks?email=${email}&key=${process.env.MP_KEY}`),
+        axios.get(`https://www.mountainproject.com/data/get-user?email=${email}&key=${process.env.MP_KEY}`)
       ])
       if(userResponse.data) {
         return { statusCode: 200, body: JSON.stringify({ name: userResponse.data.name, ticks: ticksResponse.data }) }
