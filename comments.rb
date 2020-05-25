436273beb6d318a9a741a29a32ac366f840f3f9a
Jared Dillard <jared.dillard@gmail.com>
2020-05-25 12:33:21 -0700

    Add comments script

---
+++ comments.rb
@@ -0,0 +1,136 @@
+require 'octokit'
+require 'yaml'
+require 'kramdown'
+
+# Set access token
+client = Octokit::Client.new(:access_token => COMMENTS_TOKEN)
+
+# Configure client
+client.auto_paginate = true
+
+repo = 'jdillard/blog-comments'
+
+# clear the old crags markdown files
+Dir.foreach('source/_includes/comments') do |f|
+  fn = File.join('source/_includes/comments', f)
+  File.delete(fn) if f != '.' && f != '..'
+end
+
+# Loop through blog posts and grab front matter
+Dir.glob("source/_posts/*.md") do |blog_post|
+  front_matter = YAML.load_file(blog_post)
+  # Get page.comments listed in front matter if published is true
+  if front_matter["published"] === true && front_matter["comments"]
+    File.open("source/_includes/comments/" + front_matter["comments"].to_s  + ".html","w") do |f|
+      if client.issue(repo, front_matter["comments"]).comments > 0
+        # Loop over comments and write out comment file
+        client.issue_comments(repo, front_matter["comments"]).each do |comment|
+          # count reactions for each comment
+          reactions = Hash.new
+          client.issue_comment_reactions(repo, comment["id"]).each do |reaction|
+            if reaction["content"] === "+1"
+              reactions.key?("plusOne") ? reactions["plusOne"] +=  1 : reactions["plusOne"] = 1
+            end
+            if reaction["content"] === "-1"
+              reactions["minusOne"] ? reactions["minusOne"] +=  1 : reactions["minusOne"] = 1
+            end
+            if reaction["content"] === "laugh"
+              reactions["laugh"] ? reactions["laugh"] +=  1 : reactions["laugh"] = 1
+            end
+            if reaction["content"] === "hooray"
+              reactions["hooray"] ? reactions["hooray"] +=  1 : reactions["hooray"] = 1
+            end
+            if reaction["content"] === "confused"
+              reactions["confused"] ? reactions["confused"] +=  1 : reactions["confused"] = 1
+            end
+            if reaction["content"] === "heart"
+              reactions["heart"] ? reactions["heart"] +=  1 : reactions["heart"] = 1
+            end
+            if reaction["content"] === "eyes"
+              reactions["eyes"] ? reactions["eyes"] +=  1 : reactions["eyes"] = 1
+            end
+            if reaction["content"] === "rocket"
+              reactions["rocket"] ? reactions["rocket"] +=  1 : reactions["rocket"] = 1
+            end
+          end
+          f << '<div class="comment flex flex-row">'+"\n"
+          f << '  <div class="mr3">'+"\n"
+          f << '    <a href="' + comment["user"]["html_url"] + '" target="_blank">'+"\n"
+          f << '        <img class="br2 mw3" src="' +  comment["user"]["avatar_url"] + '"/>'+"\n"
+          f << '    </a>'+"\n"
+          f << '  </div>'+"\n"
+          f << '  <div class="flex relative flex-column ba b--light-gray br1 mb3">'+"\n"
+          f << '    <div class="f6 ph3 pv2 bg-near-white bb b--light-gray gray">'+"\n"
+          f << '        <a href="' + comment["user"]["html_url"] + '" target="_blank" class="mid-gray b link">' + comment["user"]["login"] + '</a> commented on'+"\n"
+          f << '        <a href="' + comment["html_url"] + '" target="_blank" class="gray link">'+"\n"
+          f << '            <relative-time datetime="' + comment["created_at"].to_i.to_s + '">'+"\n"
+          f << '               ' + comment["created_at"].strftime("%B %d, %Y") + "\n"
+          f << '            </relative-time>'+"\n"
+          f << '        </a>&nbsp;'
+                        comment["updated_at"].to_s === "" ? f << "\n" : f << "(edited)\n"
+          f << '    </div>'+"\n"
+          f << '    <div class="body pv2 ph3 black-70">'+"\n"
+          f << '      ' + Kramdown::Document.new(comment["body"], :input => 'GFM', :syntax_highlighter => 'rouge').to_html+"\n"
+          f << '    </div>'+"\n"
+          f << '    <div class="bt b--light-gray gray">'+"\n"
+          f << '      <ul class="flex flex-wrap pl0 mv0 list">'+"\n"
+                      reactions.each do |reaction, count|
+          f << '        <li class="pv2 ph3 br b--light-gray">'+"\n"
+                        if reaction === "plusOne"
+          f << '          <g-emoji alias="+1" fallback-src="https://assets-cdn.github.com/images/icons/emoji/unicode/1f44d.png" class="v-mid">'+"\n"
+          f << '            <img class="v-mid" alt="+1" height="20" width="20" src="https://github.githubassets.com/images/icons/emoji/unicode/1f44d.png">'+"\n"
+          f << '          </g-emoji>' + count.to_s + "\n"
+                        end
+                        if reaction === "minusOne"
+          f << '          <g-emoji alias="-1" fallback-src="https://assets-cdn.github.com/images/icons/emoji/unicode/1f44e.png" class="v-mid">'+"\n"
+          f << '            <img class="v-mid" alt="-1" height="20" width="20" src="https://github.githubassets.com/images/icons/emoji/unicode/1f44e.png">'+"\n"
+          f << '          </g-emoji>' + count.to_s + "\n"
+                        end
+                        if reaction === "laugh"
+          f << '          <g-emoji alias="smile" fallback-src="https://assets-cdn.github.com/images/icons/emoji/unicode/1f604.png" class="v-mid">'+"\n"
+          f << '            <img class="v-mid" alt="smile" height="20" width="20" src="https://github.githubassets.com/images/icons/emoji/unicode/1f604.png">'+"\n"
+          f << '          </g-emoji>' + count.to_s + "\n"
+                        end
+                        if reaction === "hooray"
+          f << '          <g-emoji alias="tada" fallback-src="https://assets-cdn.github.com/images/icons/emoji/unicode/1f389.png" class="v-mid">'+"\n"
+          f << '            <img class="v-mid" alt="tada" height="20" width="20" src="https://github.githubassets.com/images/icons/emoji/unicode/1f389.png">'+"\n"
+          f << '          </g-emoji>' + count.to_s + "\n"
+                        end
+                        if reaction === "confused"
+          f << '          <g-emoji alias="thinking_face" fallback-src="https://assets-cdn.github.com/images/icons/emoji/unicode/1f615.png" class="v-mid">'+"\n"
+          f << '            <img class="v-mid" alt="thinking_face" height="20" width="20" src="https://github.githubassets.com/images/icons/emoji/unicode/1f615.png">'+"\n"
+          f << '          </g-emoji>' + count.to_s + "\n"
+                        end
+                        if reaction === "heart"
+          f << '          <g-emoji alias="heart" fallback-src="https://assets-cdn.github.com/images/icons/emoji/unicode/2764.png" class="v-mid">'+"\n"
+          f << '            <img class="v-mid" alt="heart" height="20" width="20" src="https://github.githubassets.com/images/icons/emoji/unicode/2764.png">'+"\n"
+          f << '          </g-emoji>' + count.to_s + "\n"
+                        end
+                        if reaction === "eyes"
+          f << '          <g-emoji alias="eyes" fallback-src="https://assets-cdn.github.com/images/icons/emoji/unicode/1f440.png" class="v-mid">'+"\n"
+          f << '            <img class="v-mid" alt="eyes" height="20" width="20" src="https://github.githubassets.com/images/icons/emoji/unicode/1f440.png">'+"\n"
+          f << '          </g-emoji>' + count.to_s + "\n"
+                        end
+                        if reaction === "rocket"
+          f << '          <g-emoji alias="rocket" fallback-src="https://assets-cdn.github.com/images/icons/emoji/unicode/1f680.png" class="v-mid">'+"\n"
+          f << '            <img class="v-mid" alt="rocket" height="20" width="20" src="https://github.githubassets.com/images/icons/emoji/unicode/1f680.png">'+"\n"
+          f << '          </g-emoji>' + count.to_s + "\n"
+                          end
+          f << '        </li>'+"\n"
+                      end
+          f << '      </ul>'+"\n"
+          f << '    </div>'+"\n"
+          f << '  </div>'+"\n"
+          f << '</div>'+"\n"
+          f << '<div class="pa3 tc ba b--light-gray black-70 mb3">'+"\n"
+          f << '  <a href="https://github.com/' + repo + '/issues/' + front_matter["comments"].to_s + '" class="b mid-gray">Join the discussion on GitHub</a>'+"\n"
+          f << '</div>'
+        end
+      else
+        f << '<div class="pa3 tc ba b--light-gray black-70 mb3">'+"\n"
+        f << '  There are currently no comments. <a href="https://github.com/' + repo + '/issues/' + front_matter["comments"].to_s + '" class="b mid-gray">Leave a comment on GitHub</a>'+"\n"
+        f << '</div>'
+      end
+    end
+  end
+end
