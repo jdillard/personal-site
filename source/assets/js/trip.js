62f6a4e01d08f50b7f14709dfea2f7049bb982db
Jared Dillard <jared.dillard@gmail.com>
2022-05-10 23:40:40 -0700

    Add related trips functionality

--- source/assets/js/trip.js
+++ source/assets/js/trip.js
@@ -1,4 +1,7 @@
+import u from 'umbrellajs';
+
 const resources_template = require("./templates/resources-simple.hbs");
+const trips_template = require("./templates/recent_trips.hbs");
 
 /* parallax on blog posts with cover image */
 var parallaxImage = document.getElementById('ParallaxImage');
@@ -9,6 +12,7 @@ window.addEventListener('scroll', function windowScroll() {
   parallaxImage.style.transform = 'translate3d(0, ' + windowScrolled / 4 + 'px, 0)';
 });
 
+/* Populate Get Involved section */
 axios.get('/assets/json/get-involved.json')
   .then(function (response) {
     resources(response.data);
@@ -26,6 +30,80 @@ function resources(data=[]) {
   resource_element.innerHTML = resources_template(resource_list);
 }
 
+/* Populate Recent Trips */
+axios.get('/assets/json/trips.json')
+  .then(function (response) {
+    const trips_element = document.getElementById("trips");
+    trips(response.data, trips_element.dataset.trip, false, trips_element.dataset.type);
+  })
+  .catch(function (error) {
+    // call local file on 403
+    console.log(error);
+  });
+
+function trips(trips=[], current_trip, active_trip, active_type) {
+  var types = [];
+  trips.data.forEach(function(trip) {
+    var found = types.some(el => el.type === trip.type);
+    if(!found) {
+      if(trip.type === active_type) {
+        types.push({"color": "light-red", "type": trip.type});
+      } else {
+        types.push({"color": "silver", "type": trip.type});
+      }
+    }
+  });
+  var related_trips = trips.data.reduce(function(res, trip) {
+    if(trip.type == active_type && trip.title != current_trip) {
+      if((active_trip && active_trip == trip.title) || (!active_trip && res.length == 0)) {
+        trip['border_color'] = 'light-red';
+        trip['text_color'] = 'light-red';
+      } else {
+        trip['border_color'] = 'white';
+        trip['text_color'] = 'black-70';
+      }
+      const date = new Date(trip['date_published']);
+      trip['date_published'] = date.toLocaleDateString("en-US", { year: 'numeric', month: 'long', day: 'numeric' });
+      res.push(trip);
+    }
+    return res;
+  }, []);
+  if(active_trip) {
+    var trip_info = related_trips.find(o => o.title === active_trip);;
+  } else {
+    var trip_info = related_trips[0];
+  }
+  var trips_list = {"types": types, "trips": related_trips, "trip_info": trip_info};
+  const trips_element = document.getElementById("trips");
+  trips_element.innerHTML = trips_template(trips_list);
+}
+
+/* recent trips nav */
+u("#trips").on('click', '.related-top-nav', function(node) {
+  axios.get('/assets/json/trips.json')
+    .then(function (response) {
+      const trips_element = document.getElementById("trips");
+      trips_element.dataset.type = u(node.target).data('type');
+      trips(response.data, trips_element.dataset.trip, false, u(node.target).data('type'));
+    })
+    .catch(function (error) {
+      // call local file on 403
+      console.log(error);
+    });
+})
+u("#trips").on('click', '.related-side-nav', function(node) {
+  axios.get('/assets/json/trips.json')
+    .then(function (response) {
+      const trips_element = document.getElementById("trips");
+      trips(response.data, trips_element.dataset.trip, u(node.target).data('trip'), trips_element.dataset.type);
+    })
+    .catch(function (error) {
+      // call local file on 403
+      console.log(error);
+    });
+})
+
+/* If title breaks, break it in half */
 var title = document.querySelector("h1");
 var copy = title.cloneNode(true);
     copy.innerHTML = 'A';
