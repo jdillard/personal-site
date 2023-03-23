782c637ab36c991451e1cec3ce2b00207fee5e6f
Jared Dillard <jared.dillard@gmail.com>
2023-03-22 22:56:40 -0700

    reduce time to 1.5s

--- download-avy-json.py
+++ download-avy-json.py
@@ -31,7 +31,7 @@ with open("avalanche-reports-raw/map-layer.json") as fp:
 
 # loop through each zone and download it's report if it exists
 for item in map_layer["features"]:
-    time.sleep(2)
+    time.sleep(1.5)
     center_id = item['properties']['center_id']
     zone_id = item['id']
     print(center_id, zone_id)
