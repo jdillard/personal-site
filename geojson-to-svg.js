1320f1ce956b3e82a11b90e5a5c927a666efd963
Jared Dillard <jared.dillard@gmail.com>
2023-11-11 12:15:05 -0800

    Loop through geojson files

--- geojson-to-svg.js
+++ geojson-to-svg.js
@@ -1,63 +1,72 @@
 const {GeoJSON2SVG} = require('geojson2svg');
 var fs = require('fs');
+const path = require('path')
+
+function generateSVG(dirpath, filename) {
+  const inputGeoJSON = `${dirpath}/${filename}.geojson`;
+  const dataJSONSource = fs.readFileSync(inputGeoJSON, {
+    encoding: "utf8",
+    flag: "r",
+  });
+
+  const dataJSON = JSON.parse(dataJSONSource);
+
+  const attributes = [
+    "properties.PRO_COM_T",
+    "properties.COMUNE",
+    "properties.COD_REG",
+    "properties.COD_PROV",
+  ];
+
+  const converter = new GeoJSON2SVG({
+    attributes: attributes,
+  });
+
+  const svgStr = converter.convert(dataJSON);
+
+  const svg = parseSVG(svgStr);
+
+  function parseSVG(str) {
+    const paths = `<g class="comuni" id="comuni">\r\n${str.join("\r\n")}</g>`;
+
+    return `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+    <svg
+    xmlns:dc="http://purl.org/dc/elements/1.1/"
+    xmlns:cc="http://creativecommons.org/ns#"
+    xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+    xmlns:svg="http://www.w3.org/2000/svg"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+  >
+  <style>
+  g.comuni {
+      fill: lightyellow;
+      stroke-width: 3.5;
+      stroke-linecap: square;
+      stroke-linejoin: bevel;
+      stroke-miterlimit: 3;
+      stroke-opacity: 1;
+      stroke: black;
+      fill-opacity: 0;
+  }
+  </style>
+  <g inkscape:groupmode="layer" id="layer1" inkscape:label="Base"/>
+  <g inkscape:groupmode="layer" id="layer2" inkscape:label="Comuni">
+  ${paths}
+  </g>
+  </svg>
+  `;
+  }
+
+  fs.writeFileSync(`${dirpath}/${filename}.svg`, svg);
+}
 
 //TODO dynamic input filename
-const inputGeoJSON = "./test_polyon.json";
-const dataJSONSource = fs.readFileSync(inputGeoJSON, {
-  encoding: "utf8",
-  flag: "r",
-});
+const dirpath = path.join(__dirname, './avalanche-reports-raw')
 
-const dataJSON = JSON.parse(dataJSONSource);
-
-const attributes = [
-  "properties.PRO_COM_T",
-  "properties.COMUNE",
-  "properties.COD_REG",
-  "properties.COD_PROV",
-];
-
-const converter = new GeoJSON2SVG({
-  attributes: attributes,
-});
-
-const svgStr = converter.convert(dataJSON);
-
-const svg = parseSVG(svgStr);
-
-function parseSVG(str) {
-  const paths = `<g class="comuni" id="comuni">\r\n${str.join("\r\n")}</g>`;
-
-  return `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-  <svg
-   xmlns:dc="http://purl.org/dc/elements/1.1/"
-   xmlns:cc="http://creativecommons.org/ns#"
-   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-   xmlns:svg="http://www.w3.org/2000/svg"
-   xmlns="http://www.w3.org/2000/svg"
-   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
->
-<style>
-g.comuni {
-    fill: lightyellow;
-    stroke-width: 3.5;
-    stroke-linecap: square;
-    stroke-linejoin: bevel;
-    stroke-miterlimit: 3;
-    stroke-opacity: 1;
-    stroke: black;
-    fill-opacity: 0;
-}
-</style>
-<g inkscape:groupmode="layer" id="layer1" inkscape:label="Base"/>
-<g inkscape:groupmode="layer" id="layer2" inkscape:label="Comuni">
-${paths}
-</g>
-</svg>
-`;
-}
-
-//TODO dynamic output filename
-const fileOutput = "test_polygon";
-
-fs.writeFileSync(`${fileOutput}.svg`, svg);
+fs.readdir(dirpath, function(err, files) {
+  const geosonFiles = files.filter(file => path.extname(file) === '.geojson').map(file => path.parse(file).name);
+  geosonFiles.forEach(function (filename) {
+    generateSVG(dirpath, filename);
+  });
+})
