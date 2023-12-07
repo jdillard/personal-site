06c108192bcce3a40410ad06658fa33e5535c82a
Jared Dillard <jared.dillard@gmail.com>
2023-12-06 21:39:51 -0800

    Add support for Avalanche Canada (#100)

--- caltopo-layers.py
+++ caltopo-layers.py
@@ -7,15 +7,15 @@ from slugify import slugify
 import os
 import urllib.parse
 import uuid
-from datetime import datetime
 from dateutil import tz
+import dateutil.parser
 from tzwhere import tzwhere
 import toml
 
 #TODO support search by lat/long (mainly for CO/BC) (lambda function?)
 #TODO expired reports?
 #TODO update copy?
-#TODO get pull rebase on action to prevent conflicts while the script is running?
+#TODO git pull rebase on action to prevent conflicts while the script is running?
 
 # load jinja templates
 environment = Environment(loader=FileSystemLoader("templates/"))
@@ -32,6 +32,7 @@ aspect = {
     "east": (68, 113),
     "southeast": (113, 158),
     "south": (158, 203),
+    "s": (158, 203),
     "southwest": (203, 248),
     "west": (248, 293),
     "northwest": (293, 339),
@@ -51,12 +52,19 @@ aspect_abbr = {
 
 # danger level color codes
 danger_levels = {
-    0: {"color": "939598", "desc": "No rating"},
-    1: {"color": "50b848", "desc": "Low danger"},
-    2: {"color": "fff200", "desc": "Moderate danger"},
-    3: {"color": "f7941e", "desc": "Considerable danger"},
-    4: {"color": "ed1c24", "desc": "High danger"},
-    5: {"color": "231f20", "desc": "Extreme danger"},
+    "earlyseason": {"scale": 0, "color": "939598", "desc": "Early season"},
+    "norating": {"scale": 0, "color": "939598", "desc": "No rating"},
+    "noforecast": {"scale": 0, "color": "939598", "desc": "No forecast"},
+    "low": {"scale": 1, "color": "50b848", "desc": "Low danger"},
+    "moderate": {"scale": 2, "color": "fff200", "desc": "Moderate danger"},
+    "considerable": {"scale": 3, "color": "f7941e", "desc": "Considerable danger"},
+    "high": {"scale": 4, "color": "ed1c24", "desc": "High danger"},
+    0: {"scale": 0, "color": "939598", "desc": "No rating"},
+    1: {"scale": 1, "color": "50b848", "desc": "Low danger"},
+    2: {"scale": 2, "color": "fff200", "desc": "Moderate danger"},
+    3: {"scale": 3, "color": "f7941e", "desc": "Considerable danger"},
+    4: {"scale": 4, "color": "ed1c24", "desc": "High danger"},
+    5: {"scale": 5, "color": "231f20", "desc": "Extreme danger"},
 }
 
 # problems color codes
@@ -279,7 +287,21 @@ def sort_directions(subset):
 
     return (subset_sorted[0][0],subset_sorted[-1][1], desc)
 
-# dynamically group zones by state
+
+# calculate geo center of CA areas
+with open("avalanche-reports-raw/ca-areas.json") as fp:
+    ca_areas = json.load(fp)
+
+toml_data_ca = {}
+for area in ca_areas["features"]:
+    # add each zone location to user.toml
+    toml_data_ca[area['id']] = {
+        "name": area['id'],
+        "longitude": area['properties']['centroid'][0], #TODO confirm correct order
+        "latitude": area['properties']['centroid'][1],
+    }
+
+# dynamically group US zones by state
 with open("avalanche-reports-raw/map-layer.json") as fp:
     map_layer = json.load(fp)
 
@@ -315,7 +337,7 @@ for item in map_layer["features"]:
     zone_info = {
         "zone_id": item["id"],
         "name": item["properties"]["name"],
-        "slug": item["id"],
+        "slug": f"{item['properties']['center_id']}-{item['id']}",
         "url": item["properties"]["link"],
         "center_id": item["properties"]["center_id"],
         "geo": lat_long,
@@ -343,11 +365,194 @@ for item in map_layer["features"]:
 
 with open("user.toml", "w") as toml_file:
     toml.dump(toml_data, toml_file)
+    toml.dump(toml_data_ca, toml_file)
 
 # delete all DEM files in order to start fresh
 dir = "source/avy"
 for f in os.listdir(dir):
     os.remove(os.path.join(dir, f))
+
+#TODO loop through CA areas and create DEM pages
+with open("avalanche-reports-raw/ca-metadata.json") as fp:
+    ca_metadata = json.load(fp)
+
+def find_object_by_key_value(objects_list, key, value):
+    for obj in objects_list:
+        if obj.get(key) == value:
+            return obj
+    return None  # Return None if no match is found
+areas = []
+for product in ca_metadata:
+    published = False
+    tomorrow = False
+    data = {}
+    area = {}
+    if os.path.isfile(f"avalanche-reports-raw/{product['product']['id']}.json"):
+        with open(f"avalanche-reports-raw/{product['product']['id']}.json") as fp:
+            data = json.load(fp)
+            print(product['product']['id'])
+
+        #TODO move duplicate code as US to functions
+
+        # timezone calculations
+        from_zone = tz.gettz('UTC')
+        # lookup area id in ca_areas
+        # found_value = next((d for d in ca_areas["features"] if d.get("id") == data['area']['id']), None)
+        found_value = find_object_by_key_value(ca_areas["features"], "id", data['area']['id'])
+        if not found_value:
+            continue #TODO figure out why the id different than the filename and hopefully remove
+        timezone_str = tzwhere.tzNameAt(found_value.get('properties')["centroid"][1],found_value.get('properties')["centroid"][0]) #TODO make sure correct order
+        to_zone = tz.gettz(timezone_str)
+        utc = dateutil.parser.parse(data["report"]["dateIssued"])
+        utc = utc.replace(tzinfo=from_zone)
+
+        published_date_time_obj = utc.astimezone(to_zone)
+        tomorrow_date_time_obj = published_date_time_obj + timedelta(hours=24, minutes=0)
+
+        published = published_date_time_obj.strftime("%A, %B %d, %Y %-I:%M%p")
+        tomorrow = tomorrow_date_time_obj.strftime("%Y-%m-%d")
+
+    # TODO set elevations levels
+    area["elevations"] = {
+            "lower": (6000, 7500),
+            "middle": (7501, 9000),
+            "upper": (9001, 10500),
+        }
+
+    # avy danger
+    danger_rules = []
+    danger_layer = ""
+    area_color = 0
+    if data and data["report"]["dangerRatings"] and (data["report"]["dangerRatings"][0]["ratings"]["btl"] or data["report"]["dangerRatings"][0]["ratings"]["tln"] or data["report"]["dangerRatings"][0]["ratings"]["alp"]) and area["elevations"]:
+        # print(data["report"]["dangerRatings"][0]["ratings"]["btl"]["rating"]["value"],data["report"]["dangerRatings"][0]["ratings"]["tln"]["rating"]["value"],data["report"]["dangerRatings"][0]["ratings"]["alp"]["rating"]["value"])
+        lower = f'a0-0e{area["elevations"].get("lower")[0]}-{area["elevations"].get("lower")[1]}f {danger_levels.get(data["report"]["dangerRatings"][0]["ratings"]["btl"]["rating"]["value"])["color"]}'
+        middle = f'a0-0e{area["elevations"].get("middle")[0]}-{area["elevations"].get("middle")[1]}f {danger_levels.get(data["report"]["dangerRatings"][0]["ratings"]["tln"]["rating"]["value"])["color"]}'
+        upper = f'a0-0e{area["elevations"].get("upper")[0]}-{area["elevations"].get("upper")[1]}f {danger_levels.get(data["report"]["dangerRatings"][0]["ratings"]["alp"]["rating"]["value"])["color"]}'
+        danger_rules.append({
+            "layer": lower,
+            "desc": f"{danger_levels.get(data['report']['dangerRatings'][0]['ratings']['btl']['rating']['value'])['desc']} (below {area['elevations'].get('lower')[1]}')",
+            "color": f"#{danger_levels.get(data['report']['dangerRatings'][0]['ratings']['btl']['rating']['value'])['color']}"
+        })
+        danger_rules.append({
+            "layer": middle,
+            "desc": f"{danger_levels.get(data['report']['dangerRatings'][0]['ratings']['tln']['rating']['value'])['desc']} ({area['elevations'].get('middle')[0]}' to {area['elevations'].get('middle')[1]}')",
+            "color": f"#{danger_levels.get(data['report']['dangerRatings'][0]['ratings']['tln']['rating']['value'])['color']}"
+        })
+        # only show (above x') if == 20310
+        if area['elevations'].get('upper')[1] == 20310:
+            elv_range = f"above {area['elevations'].get('upper')[0]}'"
+        else:
+            elv_range =  f"{area['elevations'].get('upper')[0]}' to {area['elevations'].get('upper')[1]}'"
+        danger_rules.append({
+            "layer": upper,
+            "desc": f"{danger_levels.get(data['report']['dangerRatings'][0]['ratings']['alp']['rating']['value'])['desc']} ({elv_range})",
+            "color": f"#{danger_levels.get(data['report']['dangerRatings'][0]['ratings']['alp']['rating']['value'])['color']}"
+        })
+        danger_layer = f"sc_{'p'.join(x['layer'].replace(' ', 'c') for x in danger_rules)}"
+        zone_color = max(danger_levels.get(data["report"]["dangerRatings"][0]["ratings"]["btl"]["rating"]["value"])["scale"], danger_levels.get(data["report"]["dangerRatings"][0]["ratings"]["tln"]["rating"]["value"])["scale"], danger_levels.get(data["report"]["dangerRatings"][0]["ratings"]["alp"]["rating"]["value"])["scale"])
+
+    # avy problems
+    problems = []
+    if data["report"]["problems"] and area["elevations"]:
+        for i, problem in enumerate(data["report"]["problems"]):
+            lower = []
+            middle = []
+            upper = []
+            rules = []
+
+            for elevation in problem["data"]["elevations"]:
+                # calculate start and stop
+                for direction in problem["data"]["aspects"]:
+                    lower.append(aspect[direction["display"].lower()])
+                start, end, desc = sort_directions(lower)
+
+                if elevation["value"] == "btl":
+                    elv_band = "lower"
+                    full_desc = f"{desc} (below {area['elevations'].get('lower')[1]}')"
+                elif elevation["value"] == "tln":
+                    elv_band = "middle"
+                    full_desc = f"{desc} ({area['elevations'].get('middle')[0]}' to {area['elevations'].get('middle')[1]}')"
+                elif elevation["value"] == "alp":
+                    # only show (above x') if == 20310
+                    elv_band = "upper"
+                    if area['elevations'].get('upper')[1] == 20310:
+                        elv_range = f"above {area['elevations'].get('upper')[0]}'"
+                    else:
+                        elv_range = f"{area['elevations'].get('upper')[0]}' to {area['elevations'].get('upper')[1]}'"
+                    full_desc = f"{desc} ({elv_range})"
+                else:
+                    continue
+
+                rules.append({
+                    "layer": f'a{start}-{end}e{area["elevations"].get(elv_band)[0]}-{area["elevations"].get(elv_band)[1]}f {problem_color.get(i)}',
+                    "desc": full_desc,
+                    "color": f"#{problem_color.get(i)}",
+                })
+
+            problems.append({
+                "name": f"{tomorrow} {problem['type']['display']}",
+                "desc": f"Problem #{i+1}",
+                "color": f"#{problem_color.get(i)}",
+                "rules": rules,
+                "layers": f"sc_{'p'.join(x['layer'].replace(' ', 'c') for x in rules)}",
+                "uuid": str(uuid.uuid4()),
+            })
+
+    area["published"] = published
+    area["danger"] = danger_rules
+    area["problems"] = problems
+    area["color"] = f"#{danger_levels[zone_color]['color']}"
+    area["geo"] = found_value['properties']['centroid']
+    area["name"] = "Canada zone"
+    area["slug"] = data['area']['id']
+    area["center_id"] = "Avalanche Canada"
+    area["url"] = f"https://avalanche.ca/forecasts/{product['product']['id']}"
+
+    # calculate sunlight angles
+    if not tomorrow:
+        tomorrow = datetime.today().strftime('%Y-%m-%d')
+    location = f"user:{data['area']['id']}"
+    df = azely.compute('Sun', location, tomorrow)
+    df1=df.query("el > 0.5") # only when the sun is above the horizon
+
+    # format sun angle table
+    area["hillshading"] = []
+    for index, row in df1.iloc[::9, :2].iterrows(): # grab every 9th row and the first two columns
+        area["hillshading"].append({
+            "time": f"{tomorrow} {index.strftime('%I:%M%p')} Shade",
+            "lighting": f"{round(row['az'])} by {round(row['el'])}",
+            "layer": f"rb_m{round(row['az'])}z{round(row['el'])}",
+            "uuid": str(uuid.uuid4()),
+        })
+
+    layer_info = layers_template.render(
+            sun_day=tomorrow,
+            danger_layer=danger_layer,
+            shade_layers=[area["hillshading"][1],area["hillshading"][int(len(area["hillshading"])/2)-1],area["hillshading"][-3]],
+            problem_layers=area["problems"],
+        )
+
+    if danger_rules:
+        area["layers"] = f"a={danger_layer}&cl={urllib.parse.quote(layer_info)}"
+        area["message"] = ""
+    else:
+        area["layers"] = f"cl={urllib.parse.quote(layer_info)}"
+        if not data and not data["danger"]:
+            area["message"] = f'Report not available. See <a class="no-underline fancy-link relative light-red" href="{area["url"]}">{area["center_id"]}</a> for more information.'
+
+    areas.append(area)
+
+content = region_template.render(
+    page="Canada",
+    sun_day=tomorrow,
+    zones=areas, #TODO get accurate collection
+    states=states, #TODO figure out what this is for
+)
+
+# create Canada DEM page
+with open(f"source/avy/canada.md", mode="w", encoding="utf-8") as message:
+    message.write(content)
+    print(f"... wrote source/avy/canada.md")
 
 # loop through states and create DEM state pages
 for state in states:
@@ -364,7 +569,7 @@ for state in states:
                 from_zone = tz.gettz('UTC')
                 timezone_str = tzwhere.tzNameAt(zone["geo"][1],zone["geo"][0])
                 to_zone = tz.gettz(timezone_str)
-                utc = datetime.strptime(data["published_time"], '%Y-%m-%dT%H:%M:%S+00:00')
+                utc = dateutil.parser.parse(data["published_time"])
                 utc = utc.replace(tzinfo=from_zone)
 
                 published_date_time_obj = utc.astimezone(to_zone)
@@ -527,6 +732,7 @@ for state in states:
                 print(f"... wrote source{state['page']}.md")
 
 # create index DEM page
+#TODO add Canada
 with open('source/avy/index.md', mode="w", encoding="utf-8") as message:
-        message.write(index_template.render(states=sorted(states, key=lambda d: d['name'])))
+        message.write(index_template.render(states=sorted(states, key=lambda d: d['name']), areas=areas))
         print(f"... wrote source/avy/index.md")
