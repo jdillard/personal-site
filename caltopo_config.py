0fb7a9745506be3061dbbba5afec8abf92bd0f36
Jared Dillard <jared.dillard@gmail.com>
2024-11-09 11:04:18 -0800

    convert state_info to yaml

--- caltopo_config.py
+++ caltopo_config.py
@@ -1,3 +1,5 @@
+import yaml
+
 # lower and upper degrees for each cardinal direction
 aspect = {
     "north": (339, 23),
@@ -48,188 +50,193 @@ problem_color = {
     2: "009AFF",
 }
 
-#TODO convert to YAML
 # limit the states that are published (active True/False)
-state_info = [
-    {
-        "abbr": "AK",
-        "name": "Alaska",
-        "elevations": {
-            0: {
-                "lower": (0, 4400),
-                "middle": (4401, 6100),
-                "upper": (6101, 20310),
-            },
-        },
-        "active": True,
-    },
-    {
-        "abbr": "AZ",
-        "name": "Arizona",
-        "elevations": {
-            0: {
-                "lower": (0, 9700),
-                "middle": (9701, 11500),
-                "upper": (11501, 20310),
-            },
-        },
-        "active": True,
-    },
-    {
-        "abbr": "CA",
-        "name": "California",
-        "elevations": {
-            0: {
-                "lower": (0, 6200),
-                "middle": (6201, 8700),
-                "upper": (8701, 20310),
-            },
-            128: {
-                "lower": (0, 7800),
-                "middle": (7801, 10000),
-                "upper": (10001, 20310),
-            },
-        },
-        "active": True,
-    },
-    {
-        "abbr": "CO",
-        "name": "Colorado",
-        "elevations": {
-            0: {
-                "lower": (0, 9500),
-                "middle": (9501, 11500),
-                "upper": (11501, 20310),
-            },
-        },
-        "active": True,
-    },
-    {
-        "abbr": "ID",
-        "name": "Idaho",
-        "elevations": {
-            0: {},
-            149: {}, #TODO fill in
-            272: {
-                "lower": (0, 4500),
-                "middle": (4501, 5500),
-                "upper": (5501, 20310),
-            },
-            153: {
-                "lower": (0, 6500),
-                "middle": (6501, 7500),
-                "upper": (7501, 20310),
-            },
-            717: {
-                "lower": (0, 7500),
-                "middle": (7501, 8500),
-                "upper": (8501, 20310),
-            },
-            714: {
-                "lower": (0, 7500),
-                "middle": (7501, 9500),
-                "upper": (9501, 20310),
-            },
-            716: {
-                "lower": (0, 7500),
-                "middle": (7501, 9000),
-                "upper": (9001, 20310),
-            },
-            716: {
-                "lower": (0, 6500),
-                "middle": (6501, 8500),
-                "upper": (8501, 20310),
-            },
-        },
-        "active": True,
-    },
-    {
-        "abbr": "MT",
-        "name": "Montana",
-        "elevations": {
-            0: {
-                "lower": (0, 4400),
-                "middle": (4401, 6100),
-                "upper": (6101, 20310),
-            },
-        },
-        "active": True,
-    },
-    {
-        "abbr": "NH",
-        "name": "New Hampshire",
-        "elevations": {
-            0: {
-                "lower": (0, 3000),
-                "middle": (3001, 5100),
-                "upper": (5101, 20310),
-            },
-        },
-        "active": True,
-    },
-    {
-        "abbr": "NM",
-        "name": "New Mexico",
-        "elevations": {
-            0: {
-                "lower": (0, 9500),
-                "middle": (9501, 11500),
-                "upper": (11501, 20310),
-            },
-        },
-        "active": True,
-    },
-    {
-        "abbr": "OR",
-        "name": "Oregon",
-        "elevations": {
-            0: {
-                "lower": (0, 4000),
-                "middle": (4001, 5000),
-                "upper": (5001, 20310),
-            },
-        },
-        "active": True,
-    },
-    {
-        "abbr": "UT",
-        "name": "Utah",
-        "elevations": {},
-        "active": True,
-    },
-    {
-        "abbr":"WA",
-        "name": "Washington",
-        "elevations": {
-            0: {
-                "lower": (0, 3900),
-                "middle": (3901, 4850),
-                "upper": (4851, 20310),
-            },
-        },
-        "active": True,
-    },
-    {
-        "abbr": "WY",
-        "name": "Wyoming",
-        "elevations": {
-            0: {},
-            1331: {
-                "lower": (0, 7501),
-                "middle": (7501, 8500),
-                "upper": (8501, 20310),
-            },
-            1329: {
-                "lower": (6000, 7500),
-                "middle": (7501, 9000),
-                "upper": (9001, 20310),
-            },
-            1330: {
-                "lower": (6000, 7500),
-                "middle": (7501, 9000),
-                "upper": (9001, 10500),
-            },
-        },
-        "active": True,
-    },
-]
+# state_info = [
+#     {
+#         "abbr": "AK",
+#         "name": "Alaska",
+#         "elevations": {
+#             0: {
+#                 "lower": (0, 4400),
+#                 "middle": (4401, 6100),
+#                 "upper": (6101, 20310),
+#             },
+#         },
+#         "active": True,
+#     },
+#     {
+#         "abbr": "AZ",
+#         "name": "Arizona",
+#         "elevations": {
+#             0: {
+#                 "lower": (0, 9700),
+#                 "middle": (9701, 11500),
+#                 "upper": (11501, 20310),
+#             },
+#         },
+#         "active": True,
+#     },
+#     {
+#         "abbr": "CA",
+#         "name": "California",
+#         "elevations": {
+#             0: {
+#                 "lower": (0, 6200),
+#                 "middle": (6201, 8700),
+#                 "upper": (8701, 20310),
+#             },
+#             128: {
+#                 "lower": (0, 7800),
+#                 "middle": (7801, 10000),
+#                 "upper": (10001, 20310),
+#             },
+#         },
+#         "active": True,
+#     },
+#     {
+#         "abbr": "CO",
+#         "name": "Colorado",
+#         "elevations": {
+#             0: {
+#                 "lower": (0, 9500),
+#                 "middle": (9501, 11500),
+#                 "upper": (11501, 20310),
+#             },
+#         },
+#         "active": True,
+#     },
+#     {
+#         "abbr": "ID",
+#         "name": "Idaho",
+#         "elevations": {
+#             0: {},
+#             149: {}, #TODO fill in
+#             272: {
+#                 "lower": (0, 4500),
+#                 "middle": (4501, 5500),
+#                 "upper": (5501, 20310),
+#             },
+#             153: {
+#                 "lower": (0, 6500),
+#                 "middle": (6501, 7500),
+#                 "upper": (7501, 20310),
+#             },
+#             717: {
+#                 "lower": (0, 7500),
+#                 "middle": (7501, 8500),
+#                 "upper": (8501, 20310),
+#             },
+#             714: {
+#                 "lower": (0, 7500),
+#                 "middle": (7501, 9500),
+#                 "upper": (9501, 20310),
+#             },
+#             716: {
+#                 "lower": (0, 7500),
+#                 "middle": (7501, 9000),
+#                 "upper": (9001, 20310),
+#             },
+#             716: {
+#                 "lower": (0, 6500),
+#                 "middle": (6501, 8500),
+#                 "upper": (8501, 20310),
+#             },
+#         },
+#         "active": True,
+#     },
+#     {
+#         "abbr": "MT",
+#         "name": "Montana",
+#         "elevations": {
+#             0: {
+#                 "lower": (0, 4400),
+#                 "middle": (4401, 6100),
+#                 "upper": (6101, 20310),
+#             },
+#         },
+#         "active": True,
+#     },
+#     {
+#         "abbr": "NH",
+#         "name": "New Hampshire",
+#         "elevations": {
+#             0: {
+#                 "lower": (0, 3000),
+#                 "middle": (3001, 5100),
+#                 "upper": (5101, 20310),
+#             },
+#         },
+#         "active": True,
+#     },
+#     {
+#         "abbr": "NM",
+#         "name": "New Mexico",
+#         "elevations": {
+#             0: {
+#                 "lower": (0, 9500),
+#                 "middle": (9501, 11500),
+#                 "upper": (11501, 20310),
+#             },
+#         },
+#         "active": True,
+#     },
+#     {
+#         "abbr": "OR",
+#         "name": "Oregon",
+#         "elevations": {
+#             0: {
+#                 "lower": (0, 4000),
+#                 "middle": (4001, 5000),
+#                 "upper": (5001, 20310),
+#             },
+#         },
+#         "active": True,
+#     },
+#     {
+#         "abbr": "UT",
+#         "name": "Utah",
+#         "elevations": {},
+#         "active": True,
+#     },
+#     {
+#         "abbr":"WA",
+#         "name": "Washington",
+#         "elevations": {
+#             0: {
+#                 "lower": (0, 3900),
+#                 "middle": (3901, 4850),
+#                 "upper": (4851, 20310),
+#             },
+#         },
+#         "active": True,
+#     },
+#     {
+#         "abbr": "WY",
+#         "name": "Wyoming",
+#         "elevations": {
+#             0: {},
+#             1331: {
+#                 "lower": (0, 7501),
+#                 "middle": (7501, 8500),
+#                 "upper": (8501, 20310),
+#             },
+#             1329: {
+#                 "lower": (6000, 7500),
+#                 "middle": (7501, 9000),
+#                 "upper": (9001, 20310),
+#             },
+#             1330: {
+#                 "lower": (6000, 7500),
+#                 "middle": (7501, 9000),
+#                 "upper": (9001, 10500),
+#             },
+#         },
+#         "active": True,
+#     },
+# ]
+
+with open("state_info.yaml", "r") as file:
+    state_info = yaml.safe_load(file)
+
+# print(data)
+# print(state_info)
